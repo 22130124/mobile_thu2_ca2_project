92aa806bf5cb5d01d4e1e7816c350484512c9536
Truong Quy Khoa <22130124@st.hcmuaf.edu.vn>
2025-05-17 19:35:26 +0700

    Chỉnh sửa tên backend giữa User và Enrollment + Thêm POST đăng ký tài khoản mới có hash mật khẩu

--- backend/src/main/java/com/onlinecourse/backend/BackendApplication.java
+++ backend/src/main/java/com/onlinecourse/backend/BackendApplication.java
@@ -1,6 +1,5 @@
 package com.onlinecourse.backend;
 
-import com.onlinecourse.backend.controller.UserContr;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
--- backend/src/main/java/com/onlinecourse/backend/controller/UserContr.java
+++ backend/src/main/java/com/onlinecourse/backend/controller/UserContr.java
@@ -1,11 +1,8 @@
 package com.onlinecourse.backend.controller;
 
-import com.onlinecourse.backend.dto.CourseProgress;
-import com.onlinecourse.backend.dto.StatisticsResponse;
-import com.onlinecourse.backend.model.User;
-import com.onlinecourse.backend.repository.UserRepository;
+import com.onlinecourse.backend.dto.UserProgress;
 import com.onlinecourse.backend.service.UserService;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,49 +11,11 @@ import java.util.List;
 @RequestMapping("/users")
 @CrossOrigin
 public class UserContr {
+    @Autowired
+    private UserService userService;
 
-    private final UserRepository userRepository;
-    private final UserService userService;
-
-    public UserContr(UserRepository userRepository, UserService userService) {
-        this.userRepository = userRepository;
-        this.userService = userService;
+    @PostMapping
+    public UserProgress createUser(@RequestBody UserProgress userProgress) {
+        return userService.createUser(userProgress);
     }
-
-    //Lấy tất cả User
-    @GetMapping
-    public List<User> getUsers() {
-        return this.userRepository.findAll();
-    }
-
-    //Lấy User theo ID
-    @GetMapping("/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable int id) {
-        return userRepository.findById(id)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
-    }
-
-    // Lấy thống kê học tập theo user
-    @GetMapping("/{userId}/statistics")
-    public StatisticsResponse getUserStatistics(@PathVariable int userId) {
-        return userService.getUserStatistics(userId);
-    }
-
-    // Lấy danh sách tiến độ khoá học theo từng user
-    @GetMapping("/{userId}/progress")
-    public List<CourseProgress> getUserCourseProgress(@PathVariable int userId) {
-        return userService.getUserCourseProgress(userId);
-    }
-
-    // Lấy tiến độ của 1 khoá học theo từng user
-    @GetMapping("/{userId}/progress/{courseId}")
-    public CourseProgress getCourseProgressByUserAndCourse(
-            @PathVariable int userId,
-            @PathVariable int courseId) {
-        return userService.getCourseProgressByUserAndCourse(userId, courseId);
-    }
-
-
-
 }
