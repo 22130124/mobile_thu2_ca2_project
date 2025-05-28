2b7526dfeff610ec34aa36304eb9c9c2a02ba9c1
LeThiThuyKieu <22130137@st.hcmuaf.edu.vn>
2025-05-28 23:40:56 +0700

    Xu ly upload anh va update infor

--- backend/src/main/java/com/onlinecourse/backend/dto/UserProgress.java
+++ backend/src/main/java/com/onlinecourse/backend/dto/UserProgress.java
@@ -1,5 +1,8 @@
 package com.onlinecourse.backend.dto;
 
+import lombok.Data;
+
+@Data
 public class UserProgress {
     private int id;
     private String name;
@@ -7,72 +10,26 @@ public class UserProgress {
     private String password;
     private String role;
     private boolean isActive;
+    private String img;
 
-    public UserProgress() {
-    }
+    public UserProgress() {}
 
     // Constructor cho việc tạo mới user (khi đăng ký)
-    public UserProgress(String name, String email, String password, String role, boolean isActive) {
+    public UserProgress(String name, String email, String password, String role, boolean isActive, String img) {
         this.name = name;
         this.email = email;
         this.password = password;
         this.role = role;
         this.isActive = isActive;
+        this.img=img;
     }
 
-    public UserProgress(int id, String name, String email, String role, boolean isActive) {
+    public UserProgress(int id, String name, String email, String role, boolean isActive, String img) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.role = role;
         this.isActive = isActive;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public boolean isActive() {
-        return isActive;
-    }
-
-    public void setActive(boolean active) {
-        isActive = active;
+        this.img= img;
     }
 }
