02ff2f035c9f5f1e55d0c1655915fc17fc5de893
huong2004 <22130095@st.hcmuaf.edu.vn>
2025-06-01 11:05:17 +0700

    Admin CRUD course - lesson

--- mobile_app/OnlineCoursesApp/app/src/main/java/com/example/onlinecoursesapp/fragments/admin/CategoryFragment.java
+++ mobile_app/OnlineCoursesApp/app/src/main/java/com/example/onlinecoursesapp/fragments/admin/CategoryFragment.java
@@ -1,4 +1,4 @@
-package com.example.onlinecoursesapp.fragments;
+package com.example.onlinecoursesapp.fragments.admin;
 
 import android.app.AlertDialog;
 import android.os.Bundle;
@@ -21,7 +21,6 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.onlinecoursesapp.R;
 import com.example.onlinecoursesapp.adapter.CategoryAdapter;
 import com.example.onlinecoursesapp.data.CategoryRepository;
-import com.example.onlinecoursesapp.fragments.admin.CoursesManagementFragment;
 import com.example.onlinecoursesapp.models.Category;
 import com.example.onlinecoursesapp.utils.CategoryCallback;
 import com.example.onlinecoursesapp.utils.CategoryListCallback;
--- mobile_app/OnlineCoursesApp/app/src/main/java/com/example/onlinecoursesapp/models/Lesson.java
+++ mobile_app/OnlineCoursesApp/app/src/main/java/com/example/onlinecoursesapp/models/Lesson.java
@@ -1,6 +1,7 @@
 package com.example.onlinecoursesapp.models;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class Lesson {
     private int id;
@@ -9,7 +10,15 @@ public class Lesson {
     private String content;
     private String youtubeVideoUrl;
     private double durationMinutes;
-    private LocalDateTime createdAt;
+    private List<Integer> createdAt;
+    private String formattedDuration;
+
+    public Lesson(String title, String content, String youtubeUrl, double duration) {
+        this.title = title;
+        this.content=content;
+        this.youtubeVideoUrl =youtubeUrl;
+        this.durationMinutes=duration;
+    }
 
     // Getters và Setters
     public int getId() { return id; }
@@ -30,6 +39,13 @@ public class Lesson {
     public double getDurationMinutes() { return durationMinutes; }
     public void setDurationMinutes(double durationMinutes) { this.durationMinutes = durationMinutes; }
 
-    public LocalDateTime getCreatedAt() { return createdAt; }
-    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+    public List<Integer> getCreatedAt() { return createdAt; }
+
+    public void setCreatedAt(List<Integer> createdAt) {
+        this.createdAt = createdAt;
+    }
+    public String getFormattedDuration() { return formattedDuration; }
+    public void setFormattedDuration(String formattedDuration) { this.formattedDuration = formattedDuration; }
+
+
 }
